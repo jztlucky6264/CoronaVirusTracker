b9d685f3cd63f73604dda820dc4ed39566fca493
jztlucky6264 <77235457+jztlucky6264@users.noreply.github.com>
2021-08-18 16:09:55 -1200

    first commit

---
+++ src/Coronatracker.jsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState, memo } from "react";
+import { RiVirusFill } from "react-icons/ri";
+const Coronatracker = () => {
+  const [data, setData] = useState("");
+  const [Search, setSearch] = useState("");
+  const [find, setFind] = useState("");
+  const Search_state = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const Searchblank = () => {
+    setFind(() => {
+      return Search;
+    });
+    setSearch("");
+  };
+
+  const getCovidData = async () => {
+    try {
+      const res = await fetch("https://data.covid19india.org/data.json");
+      const actualData = await res.json();
+      //console.log(actualData.statewise[1]);
+      setData(actualData.statewise);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCovidData();
+  }, []);
+
+  if (!data) {
+    return (
+      <div className=" input_div d-flex align-items-center justify-content-center mt-4">
+        <input
+          type="text"
+          value={Search}
+          placeholder="search state"
+          onChange={Search_state}
+        />
+        <button className="btn" onClick={Searchblank}>
+          Search
+        </button>
+      </div>
+    );
+  }
+  return (
+    <>
+      <div className=" input_div d-flex align-items-center justify-content-center mt-4">
+        <input
+          type="text"
+          value={Search}
+          placeholder="search state"
+          onChange={Search_state}
+        />
+        <button className="btn" onClick={Searchblank}>
+          Search
+        </button>
+      </div>
+      <div className="container-fluid ">
+        <div className="row">
+          {data
+            .filter((val) => {
+              if (
+                val.state.toLocaleLowerCase().includes(find.toLocaleLowerCase())
+              ) {
+                return val;
+              } else if (setSearch === "") {
+                return setData([0]);
+              }
+            })
+            .map((states) => {
+              return (
+                <>
+                  <div className="col-sm-8 col-md-3  ">
+                    <h2>Country</h2>
+                    <h1>INDIA</h1>
+                  </div>
+                  <div className="col-sm-8 col-md-3">
+                    <h2>State </h2>
+                    <h1>{states.state}</h1>
+                  </div>
+                  <div className="col-sm-8 col-md-3">
+                    <h2>Active Cases</h2>
+                    <h1>{states.active}</h1>
+                  </div>
+                  <div className="col-sm-8 col-md-3">
+                    <h2>Confirmed Cases</h2>
+                    <h1>{states.confirmed}</h1>
+                  </div>
+                  <div className="col-sm-8 col-md-3">
+                    <h2>Recover Cases</h2>
+                    <h1>{states.recovered}</h1>
+                  </div>
+                  <div className="col-sm-8  col-md-3  ">
+                    <h2>Death</h2>
+                    <h1>{states.deaths}</h1>
+                  </div>
+                </>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default memo(Coronatracker);
---
+++ src/Covid.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Covid = () => {
+  return (
+    <>
+      <div className="global">
+        <span className="heading">🔴 LIVE</span>
+        <h1 className="heading">COVID-19 TRACKER</h1>
+      </div>
+    </>
+  );
+};
+
+export default Covid;
