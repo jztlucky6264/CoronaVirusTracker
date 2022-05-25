fa87a20a2e9a2e3551b2207cb4f809fb5627dbc5
jztlucky6264 <77235457+jztlucky6264@users.noreply.github.com>
2022-05-25 21:29:57 +0530

    update commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Coronatracker from "./Coronatracker";
 import Covid from "./Covid";
 
 const App = () => {
+  const date = new Date().getFullYear();
   return (
     <>
       <Covid />
       <Coronatracker />
+      <div className="footer">
+        <h3> © CopyRight to This site {date} </h3>
+      </div>
     </>
   );
 };
